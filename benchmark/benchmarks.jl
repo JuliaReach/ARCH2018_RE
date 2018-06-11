b2e36047543d198844773d5b67723f1b55bae4aa
mforets <marcelo.forets-irurtia@univ-grenoble-alpes.fr>
2018-06-11 09:21:56 -0300

    add benchmarks.jl

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,17 @@
+using BenchmarkTools, ARCH2018_RE
+SUITE = BenchmarkGroup()
+
+include("../src/SpaceStation/iss.jl")
+include("../src/Building/build.jl")
+
+# From BenchmarkTools.jl
+# ----------------------
+paramspath = joinpath(dirname(@__FILE__), "params.json")
+
+if isfile(paramspath)
+    loadparams!(SUITE, BenchmarkTools.load(paramspath)[1], :evals);
+else
+    tune!(SUITE)
+    BenchmarkTools.save(paramspath, params(SUITE));
+end
+

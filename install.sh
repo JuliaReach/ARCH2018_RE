b249418d9621add6bdcd165789c4047aeeabb907
Marcelo Forets <mforets@gmail.com>
2018-06-11 11:58:12 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-julia --color=yes -e 'Pkg.clone("https://github.com/JuliaReach/Reachability.jl")'
-julia --color=yes -e 'Pkg.clone("https://github.com/JuliaReach/ARCH2018_RE.jl")'
+julia --color=yes -e 'Pkg.clone("https://github.com/JuliaReach/Reachability.git")'
+julia --color=yes -e 'Pkg.clone("https://github.com/JuliaReach/ARCH2018_RE.git")'
 julia --color=yes -e 'Pkg.test("Reachability")'

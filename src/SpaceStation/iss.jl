c4fe668ba4e8c8aa58f905b02205abf898d11615
mforets <mforets@gmail.com>
2019-02-20 10:31:48 -0300

    add working iss in develop branch

--- src/SpaceStation/iss.jl
+++ src/SpaceStation/iss.jl
@@ -1,4 +1,7 @@
 using MAT, Reachability, MathematicalSystems
+using SparseArrays
+
+SUITE = BenchmarkGroup()
 
 SUITE["ISS"] = BenchmarkGroup()
 
@@ -6,7 +9,9 @@ SUITE["ISS"] = BenchmarkGroup()
 # Load model
 # ==============================
 file = matopen(@relpath "iss.mat")
-A, B, C = sparse(read(file, "A")), read(file, "B"), Matrix(read(file, "C")[3, :]')
+A = sparse(read(file, "A"))
+B = read(file, "B")
+C = Matrix(read(file, "C")[3, :]')
 n = size(A, 1)
 Cvec = C[:]
 time_horizon = 20.0
@@ -25,22 +30,22 @@ problem_TV = InitialValueProblem(S, X0)
 ISU01 = LinearConstraintProperty([Clause(LinearConstraint(Cvec, 0.0005)), Clause(LinearConstraint(-Cvec, 0.0005))])
 ISS01 = LinearConstraintProperty([Clause(LinearConstraint(Cvec, 0.0007)), Clause(LinearConstraint(-Cvec, 0.0007))])
 
-sol = solve(problem_TV, :T=>time_horizon, :δ=>5e-3, :vars=>136:270, :mode=>"check", :property=>ISU01, :projection_matrix=>C, :assume_sparse=>true)
-SUITE["ISS"]["ISU01", "dense"] = @benchmarkable solve($problem_TV, :T=>$time_horizon, :δ=>5e-3, :vars=>136:270, :mode=>"check", :property=>$ISU01, :projection_matrix=>$C, :assume_sparse=>true)
+sol = solve(problem_TV, Options(:T=>time_horizon, :mode=>"check", :property=>ISU01, :projection_matrix=>C), op=BFFPSV18(:δ=>5e-3, :vars=>136:270, :assume_sparse=>true))
+SUITE["ISS"]["ISU01", "dense"] = @benchmarkable solve($problem_TV, Options(:T=>$time_horizon, :mode=>"check", :property=>$ISU01, :projection_matrix=>$C), op=BFFPSV18(:δ=>5e-3, :vars=>136:270, :assume_sparse=>true))
 
-sol = solve(problem_TV, :T=>time_horizon, :δ=>6e-4, :vars=>136:270, :mode=>"check", :property=>ISS01, :projection_matrix=>C, :assume_sparse=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270])
-SUITE["ISS"]["ISS01", "dense"] = @benchmarkable solve($problem_TV, :T=>$time_horizon, :δ=>6e-4, :vars=>136:270, :mode=>"check", :property=>$ISS01, :projection_matrix=>$C, :assume_sparse=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270])
+sol = solve(problem_TV, Options(:T=>time_horizon, :mode=>"check", :property=>ISS01, :projection_matrix=>C), op=BFFPSV18(:δ=>6e-4, :vars=>136:270, :assume_sparse=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270]))
+SUITE["ISS"]["ISS01", "dense"] = @benchmarkable solve(problem_TV, Options(:T=>time_horizon, :mode=>"check", :property=>ISS01, :projection_matrix=>C), op=BFFPSV18(:δ=>6e-4, :vars=>136:270, :assume_sparse=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270]))
 
-sol = solve(problem_TV, :approx_model=>"nobloating", :T=>time_horizon, :δ=>5e-3, :vars=> 136:270, :mode=>"check", :property=>ISU01, :projection_matrix=>C, :assume_sparse=>true)
-SUITE["ISS"]["ISU01", "discrete"] = @benchmarkable solve($problem_TV, :approx_model=>"nobloating", :T=>$time_horizon, :δ=>5e-3, :vars=> 136:270, :mode=>"check", :property=>$ISU01, :projection_matrix=>$C, :assume_sparse=>true)
+sol = solve(problem_TV, Options(:T=>time_horizon, :mode=>"check", :property=>ISU01, :projection_matrix=>C), op=BFFPSV18(:approx_model=>"nobloating", :δ=>5e-3, :vars=> 136:270, :assume_sparse=>true))
+SUITE["ISS"]["ISU01", "discrete"] = @benchmarkable solve($problem_TV, Options(:T=>$time_horizon, :mode=>"check", :property=>$ISU01, :projection_matrix=>$C), op=BFFPSV18(:approx_model=>"nobloating", :δ=>5e-3, :vars=>136:270, :assume_sparse=>true))
 
-sol = solve(problem_TV, :approx_model=>"nobloating", :T=>time_horizon, :δ=>5e-3, :vars=>136:270, :mode=>"check", :property=>ISS01, :projection_matrix=>C, :assume_sparse=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270])
-SUITE["ISS"]["ISS01", "discrete"] = @benchmarkable solve($problem_TV, :approx_model=>"nobloating", :T=>$time_horizon, :δ=>5e-3, :vars=>136:270, :mode=>"check", :property=>$ISS01, :projection_matrix=>$C, :assume_sparse=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270])
+sol = solve(problem_TV, Options(:T=>time_horizon, :mode=>"check", :property=>ISS01, :projection_matrix=>C), op=BFFPSV18(:approx_model=>"nobloating", :δ=>5e-3, :vars=>136:270, :assume_sparse=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270]))
+SUITE["ISS"]["ISS01", "discrete"] = @benchmarkable solve($problem_TV, Options(:T=>$time_horizon, :mode=>"check", :property=>$ISS01, :projection_matrix=>$C), op=BFFPSV18(:approx_model=>"nobloating", :δ=>5e-3, :vars=>136:270, :assume_sparse=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270]))
 
 # ==============================
 # Constant input
 # ==============================
-import Reachability.add_dimension
+using Reachability:add_dimension
 A = sparse(read(file, "A"))
 Aext = add_dimension(A, 3)
 Aext[1:270, 271:273] = B
@@ -56,14 +61,14 @@ Cvec = C[1, :]
 ISU02 = LinearConstraintProperty([Clause(LinearConstraint(Cvec, 0.00017)), Clause(LinearConstraint(-Cvec, 0.00017))])
 ISS02 = LinearConstraintProperty([Clause(LinearConstraint(Cvec, 0.0005)), Clause(LinearConstraint(-Cvec, 0.0005))])
 
-sol = solve(problem_CONST, :T=>time_horizon, :δ=>5e-3, :vars=>136:270, :mode=>"check", :property=>ISU02, :projection_matrix=>C, :assume_sparse=>true, :assume_homogeneous=>true)
-SUITE["ISS"]["ISU02", "dense"] = @benchmarkable solve($problem_CONST, :T=>$time_horizon, :δ=>5e-3, :vars=>136:270, :mode=>"check", :property=>$ISU02, :projection_matrix=>$C, :assume_sparse=>true, :assume_homogeneous=>true)
+sol = solve(problem_CONST, Options(:T=>time_horizon, :mode=>"check", :property=>ISU02, :projection_matrix=>C), op=BFFPSV18(:δ=>5e-3, :vars=>136:270, :assume_sparse=>true, :assume_homogeneous=>true))
+SUITE["ISS"]["ISU02", "dense"] = @benchmarkable solve($problem_CONST, Options(:T=>$time_horizon, :mode=>"check", :property=>$ISU02, :projection_matrix=>$C), op=BFFPSV18(:δ=>5e-3, :vars=>136:270, :assume_sparse=>true, :assume_homogeneous=>true))
 
-sol = solve(problem_CONST, :T=>time_horizon, :δ=>5e-3, :vars=> 136:270, :mode=>"check", :property=>ISS02, :projection_matrix=>C, :assume_sparse=>true, :assume_homogeneous=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270])
-SUITE["ISS"]["ISS02", "dense"] = @benchmarkable solve($problem_CONST, :T=>$time_horizon, :δ=>5e-3, :vars=> 136:270, :mode=>"check", :property=>$ISS02, :projection_matrix=>$C, :assume_sparse=>true, :assume_homogeneous=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270])
+sol = solve(problem_CONST, Options(:T=>time_horizon, :mode=>"check", :property=>ISS02, :projection_matrix=>C), op=BFFPSV18(:δ=>5e-3, :vars=> 136:270, :assume_sparse=>true, :assume_homogeneous=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270]))
+SUITE["ISS"]["ISS02", "dense"] = @benchmarkable solve($problem_CONST, Options(:T=>$time_horizon, :mode=>"check", :property=>$ISS02, :projection_matrix=>$C), op=BFFPSV18(:δ=>5e-3, :vars=> 136:270, :assume_sparse=>true, :assume_homogeneous=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270]))
 
-sol = solve(problem_CONST, :approx_model=>"nobloating", :T=>time_horizon, :δ=>5e-3, :vars=>136:270, :mode=>"check", :property=>ISU02, :projection_matrix=>C, :assume_sparse=>true, :assume_homogeneous=>true)
-SUITE["ISS"]["ISU02", "discrete"] = @benchmarkable solve($problem_CONST, :approx_model=>"nobloating", :T=>$time_horizon, :δ=>5e-3, :vars=>136:270, :mode=>"check", :property=>$ISU02, :projection_matrix=>$C, :assume_sparse=>true, :assume_homogeneous=>true)
+sol = solve(problem_CONST, Options(:T=>time_horizon, :mode=>"check", :property=>ISU02, :projection_matrix=>C), op=BFFPSV18(:approx_model=>"nobloating", :δ=>5e-3, :vars=>136:270, :assume_sparse=>true, :assume_homogeneous=>true))
+SUITE["ISS"]["ISU02", "discrete"] = @benchmarkable solve($problem_CONST, Options(:T=>$time_horizon, :mode=>"check", :property=>$ISU02, :projection_matrix=>$C), op=BFFPSV18(:approx_model=>"nobloating", :δ=>5e-3, :vars=>136:270, :assume_sparse=>true, :assume_homogeneous=>true))
 
-sol = solve(problem_CONST, :approx_model=>"nobloating", :T=>time_horizon, :δ=>5e-3, :vars=> 136:270, :mode=>"check", :property=>ISS02, :projection_matrix=>C, :assume_sparse=>true, :assume_homogeneous=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270])
-SUITE["ISS"]["ISS02", "discrete"] = @benchmarkable solve($problem_CONST, :approx_model=>"nobloating", :T=>$time_horizon, :δ=>5e-3, :vars=> 136:270, :mode=>"check", :property=>$ISS02, :projection_matrix=>$C, :assume_sparse=>true, :assume_homogeneous=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270])
+sol = solve(problem_CONST, Options(:T=>time_horizon, :mode=>"check", :property=>ISS02, :projection_matrix=>C), op=BFFPSV18(:approx_model=>"nobloating", :δ=>5e-3, :vars=>136:270, :assume_sparse=>true, :assume_homogeneous=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270]))
+SUITE["ISS"]["ISS02", "discrete"] = @benchmarkable solve($problem_CONST, Options(:T=>$time_horizon, :mode=>"check", :property=>$ISS02, :projection_matrix=>$C), op=BFFPSV18(:approx_model=>"nobloating", :δ=>5e-3, :vars=>136:270, :assume_sparse=>true, :assume_homogeneous=>true, :lazy_inputs_interval=>-1, :partition=>[1:135, 136:270]))

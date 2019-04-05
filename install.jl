9aa76ea4e27e1fb1174657c51ae516be78179ddc
mforets <mforets@gmail.com>
2019-04-05 01:52:05 -0300

    use try .. catch to workaround fail

--- install.jl
+++ install.jl
@@ -12,8 +12,11 @@ Pkg.clone("https://github.com/JuliaReach/SX.jl.git");
 repo = LibGit2.GitRepo(joinpath(Pkg.dir(), "SX"));
 LibGit2.branch!(repo, "Pin_0bee1", "0bee143c44c1e6d9210e6754910f03bca7ad1a11");
 
-Pkg.clone("https://github.com/JuliaReach/Reachability.jl.git");
-repo = LibGit2.GitRepo(joinpath(Pkg.dir(), "Reachability"));
-LibGit2.branch!(repo, "Pin_73f9e", "73f9ee92342e4d46418589d34200c5682bcb8609");
+try
+    Pkg.clone("https://github.com/JuliaReach/Reachability.jl.git");
+catch
+    repo = LibGit2.GitRepo(joinpath(Pkg.dir(), "Reachability"));
+    LibGit2.branch!(repo, "Pin_73f9e", "73f9ee92342e4d46418589d34200c5682bcb8609");
+end
 
 Pkg.clone("https://github.com/JuliaReach/ARCH2018_RE.git")'
